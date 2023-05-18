ec07e93e209b3f76384fcdfdc8e217b53cdfbebb
Elise Shaffer <elise.shaffer@hey.com>
2023-05-17 21:00:10 -0600

    Follow currently opened file in neotree

--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -71,6 +71,7 @@ require('neo-tree').setup({
       },
     },
     filesystem = {
+      follow_current_file = true,
       filtered_items = {
         visible = true, -- when true, they will just be displayed differently than normal items
         hide_dotfiles = false,
