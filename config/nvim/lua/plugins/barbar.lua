b529b1528f2178eb18a185a294309996e8f58967
Elise Shaffer <elise.shaffer@hey.com>
2023-05-24 09:00:48 -0600

    Fix neo-tree barbar config

--- config/nvim/lua/plugins/barbar.lua
+++ config/nvim/lua/plugins/barbar.lua
@@ -6,8 +6,8 @@ require('barbar').setup({
       -- Or, specify the text used for the offset:
       undotree = {text = 'undotree'},
       -- Or, specify the event which the sidebar executes when leaving:
-      -- ['neo-tree'] = {event = 'BufWipeout'},
-      ['neo-tree'] = true,
+      ['neo-tree'] = {event = 'BufWipeout'},
+      -- ['neo-tree'] = true,
       -- Or, specify both
       Outline = {event = 'BufWinLeave', text = 'symbols-outline'},
     },
