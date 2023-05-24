2d882fef83b0e159a200c81d8778cb5df90fc06c
Elise Shaffer <elise.shaffer@hey.com>
2023-05-23 21:29:30 -0600

    Add lua LSP configuration

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -24,7 +24,29 @@ nvim_lsp.solargraph.setup{
   capabilities = capabilities
 }
 
-nvim_lsp.steep.setup{}
+nvim_lsp.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+-- nvim_lsp.steep.setup{}
 
 nvim_lsp.cssls.setup{
   capabilities = capabilities
