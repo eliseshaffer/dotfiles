5f63000bbfb5c4e190cb5bd6736489984c7dffd3
Elise Shaffer <elise.shaffer@hey.com>
2023-05-24 14:28:42 -0600

    Add text for NeoTree to barbar

--- config/nvim/lua/plugins/barbar.lua
+++ config/nvim/lua/plugins/barbar.lua
@@ -6,7 +6,7 @@ require('barbar').setup({
       -- Or, specify the text used for the offset:
       undotree = {text = 'undotree'},
       -- Or, specify the event which the sidebar executes when leaving:
-      ['neo-tree'] = {event = 'BufWipeout'},
+      ['neo-tree'] = {event = 'BufWipeout', text = "              File Explorer"},
       -- ['neo-tree'] = true,
       -- Or, specify both
       Outline = {event = 'BufWinLeave', text = 'symbols-outline'},
