088712d01c4cbfd4e872ca885fd62c391bd348cf
Elise Shaffer <elise.shaffer@hey.com>
2023-06-05 22:34:56 -0600

    Remove unused argument

--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -4,7 +4,7 @@ require('neo-tree').setup({
   event_handlers = {
     {
       event = "neo_tree_buffer_enter",
-      handler = function(arg)
+      handler = function()
         vim.cmd [[
           setlocal relativenumber
         ]]
